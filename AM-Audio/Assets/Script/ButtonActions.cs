efffdce849f99cde54bd9e15cf519cc4d530e084
Amgad Morsy <42896629+amgadkamal@users.noreply.github.com>
2020-03-17 14:18:22 -0600

    HW9.1

---
+++ AM-Audio/Assets/Script/ButtonActions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonActions : MonoBehaviour
+{
+    [SerializeField] 
+    AudioSource audioSource;
+
+    [SerializeField] 
+    Text rollText;
+
+    public void rollDice(int V)
+    { audioSource.Play(); 
+        rollText.text = "  you rolled a " + V.ToString(); }
+    
+    public void Attack(int W)
+    { audioSource.Play();
+        rollText.text = "  you did " + W.ToString() + " damage"; }
+    
+    public void Heal(int Z)
+    { audioSource.Play();
+        rollText.text = "  you recovered " + Z.ToString() + " health"; }
+}
+
